9cad01e0b6bf260f5847448e2b5babc944ab4c9f
FabianLamas <fabian.lamas2.0@gmail.com>
2020-04-09 09:15:45 -0300

    first commit

---
+++ public/js/auth/autenticacion.js
@@ -0,0 +1,71 @@
+class Autenticacion {
+  autEmailPass (email, password) {
+    firebase.auth().signInWithEmailAndPassword(email, password).then( result => {
+      if(result.user.emailVerified){
+        $('#avatar').attr('src', 'imagenes/usuario_auth.png')
+        Materialize.toast(`Bienvenido ${result.user.displayName}`, 5000)
+      }else{
+        firebase.auth.signOut();
+        Materialize.toast(`Por favor realiza la verificacion de la`, 5000)
+      }
+    });
+
+    $('.modal').modal('close')
+  }
+
+  crearCuentaEmailPass (email, password, nombres) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(result => {
+        result.user.updateProfile({
+          displayName: nombres
+        })
+
+        const configuracion = {
+          url: 'http://localhost:3000/'
+        }
+
+        result.user.sendEmailVerification(configuracion).catch(error => {
+          console.error(error)
+          Materialize.toast(error.message, 4000)
+        })
+
+        firebase.auth().signOut()
+
+        Materialize.toast(
+          `Bienvenido ${nombres}, debes realizar el proceso de verificación`,
+          4000
+        )
+
+        $('.modal').modal('close')
+      })
+      .catch(error => {
+        console.error(error)
+        Materialize.toast(error.message, 4000)
+      })
+  }
+
+  authCuentaGoogle () {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then( result => {
+      $('#avatar').attr('src', result.user.photoURL);
+      $('.modal').modal('close');
+      Materialize.toast(`Bienvenido ${result.user.displayName} !! `, 4000);
+    }).catch( error => {
+      console.log(error);
+      Materialize.toast(`Se ha producido un error ${error} !! `, 4000);
+    });
+  }
+
+  authCuentaFacebook () {
+    // $('#avatar').attr('src', result.user.photoURL)
+    // $('.modal').modal('close')
+    // Materialize.toast(`Bienvenido ${result.user.displayName} !! `, 4000)
+  }
+
+  authTwitter () {
+    // TODO: Crear auth con twitter
+  }
+}
